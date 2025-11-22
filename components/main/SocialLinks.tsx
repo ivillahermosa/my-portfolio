5808ca0a0b25441827e837ef0f2dde7c23bcc6cc
Ivan Villahermosa <villahermosaivan82@gmail.com>
2025-11-22 20:08:12 +0800

    Add pdf resume

--- components/main/SocialLinks.tsx
+++ components/main/SocialLinks.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { Button } from "../ui/button";
 import { DownloadIcon } from "../icons/DownloadIcon";
 import { ThemeToggle } from "../ui/ThemeToggle";
 import { SendIconFilled } from "../icons/SendIconFilled";
 
-export default async function SocialLinks() {
+export default function SocialLinks() {
     return (
         <div className="w-full h-full mx-auto px-1">
             <div className="flex flex-row md:flex-col-reverse justify-between gap-2 h-full">
@@ -17,7 +19,9 @@ export default async function SocialLinks() {
                         <span className="text-sm md:hidden font-semibold">Contact Me</span>
                     </Button>
 
-                    <Button variant="outline" className="flex items-center gap-2 min-w-0 flex-1 p-1.5 !px-4 bg-zinc-100 border-zinc-300 
+                    <Button 
+                        onClick={() => window.open("/IvanVillahermosa_FullstackNetDeveloper_6YOE.pdf", "_blank")}
+                        variant="outline" className="flex items-center gap-2 min-w-0 flex-1 p-1.5 !px-4 bg-zinc-100 border-zinc-300 
                         hover:bg-zinc-200 text-zinc-800 dark:text-white dark:bg-zinc-800 cursor-pointer border 
                         hover:border-white transition-colors duration-200">
                         <DownloadIcon className="h-5 w-5 flex-shrink-0" />
