26ef55ffe8054b691e27cb3e5744995f9bd21c49
Ivan Villahermosa <villahermosaivan82@gmail.com>
2025-11-22 17:21:47 +0800

    Add initial contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ContactPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Contact Page</h1>
+    </main>
+  );
+}
