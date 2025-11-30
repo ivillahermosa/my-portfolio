4fd1279641571d5d96f4f8455b6cacd346bd9c32
Ivan Villahermosa <villahermosaivan82@gmail.com>
2025-11-30 23:00:40 +0800

    Add back button component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,21 @@
+import { BackButton } from '@/components/ui/back';
 import React from 'react';
 
 export default function ContactPage() {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Contact Page</h1>
+    <main className="mx-auto max-w-7xl p-4">
+      <div className="max-w-7xl mx-auto p-4">
+          <BackButton/>
+
+          <div className='p-4'>         
+            <h1 className="text-4xl font-bold mb-4">
+                Get In Touch
+            </h1>
+            <p>
+                I'm actively seeking new opportunities. Let's connect and discuss how I can contribute to your team.
+            </p>
+          </div>            
+      </div>
     </main>
   );
 }
---
+++ components/ui/back.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ArrowLeft } from "lucide-react";
+import { Button } from "./button";
+import { useRouter } from "next/navigation";
+
+interface BackButtonProps {
+    label?: string;
+}
+
+export function BackButton({label = "Back"} : BackButtonProps)
+{
+    const router = useRouter();
+
+    return (
+        <Button
+            variant="ghost"
+            onClick={() => router.push("/")}
+            className="group flex items-center gap-2 text-slate-600 hover:text-slate-900 
+                dark:text-white px-0 cursor-pointer"
+        >
+            <ArrowLeft className="w-24 h-24 group-hover:-translate-x-1 transition-transform" />
+            <span className="font-medium">{label}</span>
+        </Button>
+    );
+}
